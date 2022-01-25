c8bf17fd4c59e2a34fc1e0eb52e9a3b8d69216aa
nuatli <nuatli@havelsan.com.tr>
2022-01-25 14:15:16 +0300

    AuthenticationContext

---
+++ src/shared/AuthenticationContext.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+
+export const Authentication = React.createContext();
+
+class AuthenticationContext extends Component {
+	state = {
+			isLoggedIn:false,
+			username:undefined,
+			displayName:undefined,
+			image:undefined,
+			password:undefined
+	};
+	
+	onLoginSuccess = authState => {
+		this.setState({...authState,isLoggedIn:true});
+	}
+	
+	onLogoutSuccess = () => {
+		this.setState({username:undefined,isLoggedIn:false});
+	}
+	render(){
+		return(
+				<Authentication.Provider value={{
+					state:{...state},
+					onLoginSuccesss:this.onLoginSuccesss,
+					onLogoutSuccess:this.onLogoutSuccess
+				}}>
+					{this.props.chieldren}
+				</Authentication.Provider>
+		)
+	}
+}
+
+export AuthenticationContext;
