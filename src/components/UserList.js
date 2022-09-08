a1fd89fc15889e961a8203a3ee82fa51769feac4
nuatli <nuatli@havelsan.com.tr>
2022-09-08 18:13:33 +0300

    88

---
+++ src/components/UserList.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import { getUsers } from '../api/apiCalls';
+import{withTranslation} from 'react-i18next'; 
+
+class  UserList extends Component {
+
+	state = {
+		users:[]
+	} 
+
+
+	componentDidMount(){
+		
+		getUsers().then(response =>{
+			this.setState({users:response.data});
+		})
+		
+	} 
+
+	render(){
+		const { users } = this.state;
+		const { t }  = this.props;
+		return(
+
+			<div className="card">
+				<h3 className="card-header text-center">{t('Users')}</h3>
+				<div className="list-group-flush">
+					{
+						users.map( 
+							(user,index) => (<div className="list-group-item list-group-item-action" key={user.username} >{user.username}</div>)  
+						)
+					}
+				</div>	
+			</div>
+		)
+	}
+};
+
+export default withTranslation()(UserList);
